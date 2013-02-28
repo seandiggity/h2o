55116205cba3a4a9f19c5fbe71ceb4e6b6cfb981
Tim Case <tim@endpoint.com>
2013-02-27 17:24:57 -0800

    Add versioning

--- lib/auth_utilities.rb
+++ lib/auth_utilities.rb
@@ -35,7 +35,7 @@ module AuthUtilities
     editor_list = self.accepted_roles.reject{|r| r.name != 'editor'}
     (editor_list.blank?) ? [] : editor_list.first.users.compact.uniq
   end
-
+  
   def author
     owner_list = self.accepted_roles.reject{|r| r.name != 'owner'}
     (owner_list.blank?) ? nil : owner_list.first.users.compact.uniq.first.login.downcase
