2099292e06db87d48f2d124f8023fbdc1ac6cbc0
Tim Case <tim@endpoint.com>
2013-03-27 13:28:48 -0700

    add push method to playlist

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -104,4 +104,80 @@ class Playlist < ActiveRecord::Base
     end
     actual_objects.include?(item)
   end
+  
+  def push!(options = {})
+    if options[:recipient]
+      push_to_recipient!(options[:recipient])
+    elsif options[:recipients]
+      options[:recipients].each do |r|
+        push_to_recipient!(r)
+      end
+    else
+      false
+    end
+  end           
+  
+  private
+  
+  def push_to_recipient!(recipient)
+     playlist = self
+     indent = "\t"
+
+     cloned_playlist = playlist.clone    
+     cloned_playlist.save!
+     cloned_playlist.accepts_role!(:owner, recipient)
+     cloned_playlist.accepts_role!(:creator, recipient)
+     Rails.cache.delete("user-playlists-#{recipient.id}")
+     cloned_playlist.tag_list = playlist.tag_list
+
+     playlist.playlist_items.each do |pi|
+       cloned_playlist_item = pi.clone 
+       cloned_playlist_item.save!
+       cloned_playlist_item.accepts_role!(:owner, recipient)
+       cloned_resource_item = pi.resource_item.clone
+       cloned_resource_item.save!
+       cloned_resource_item.accepts_role!(:owner, recipient)
+       if pi.resource_item.actual_object
+         if pi.resource_item_type == 'ItemPlaylist'
+           puts "#{indent}cloning playlist: #{pi.resource_item.actual_object}"
+           cloned_object = deep_clone(pi.resource_item.actual_object, recipients, "#{indent}\t")
+         else
+           puts "#{indent}cloning item: #{pi.resource_item.actual_object}"
+           cloned_object = pi.resource_item.actual_object.clone
+           cloned_object.save!
+           cloned_object.accepts_role!(:owner, recipient)
+           cloned_object.accepts_role!(:creator, recipient)
+           cloned_object.tag_list = pi.resource_item.actual_object.tag_list
+           cloned_object.save
+           if pi.resource_item_type == 'ItemCollage'
+             pi.resource_item.actual_object.annotations.each do |annotation|
+             cloned_annotation = annotation.clone
+             cloned_annotation.collage_id = cloned_object.id
+             cloned_annotation.save
+             #cloned_annotation.tag_list = annotation.tag_list
+             #Assigning tag_list doesn't work for annotations
+             #So, work directly with taggings array of objects
+             annotation.taggings.each do |tagging|
+               cloned_tagging = tagging.clone
+               cloned_tagging.update_attribute(:taggable_id, cloned_annotation.id)
+             end
+           end
+           puts "#{indent}*cloned annotations"
+          end
+        end
+        cloned_resource_item.actual_object_id = cloned_object.id
+        cloned_resource_item.url = cloned_resource_item.url.gsub(/[0-9]+$/, cloned_object.id.to_s)
+      end
+      cloned_resource_item.save
+      cloned_playlist_item.position = pi.position
+      cloned_playlist_item.resource_item_id = cloned_resource_item.id
+      cloned_playlist_item.playlist_id = cloned_playlist.id
+      cloned_playlist_item.save
+    end
+
+    cloned_playlist.save
+
+    return cloned_playlist
+    
+  end
 end
