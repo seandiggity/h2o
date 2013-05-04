c18216e60331156017d08152cdc7bdbc954cedaf
Tim Case <tim@endpoint.com>
2013-05-03 17:45:28 -0700

    add recursive child method

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -33,7 +33,7 @@ class Playlist < ActiveRecord::Base
     text :description
     text :name
     string :tag_list, :stored => true, :multiple => true
-    string :author 
+    string :author
 
     boolean :public
     boolean :active
@@ -50,6 +50,7 @@ class Playlist < ActiveRecord::Base
   def bookmark_name
     self.name
   end
+
 
   def parents
     ItemPlaylist.find_all_by_actual_object_id(self.id).collect { |p| p.playlist_item.playlist_id }.uniq
@@ -74,12 +75,24 @@ class Playlist < ActiveRecord::Base
     i = 0
     while i < r.size
       Playlist.find(r[i]).parents.each do |a|
-        next if r.include?(a) 
+        next if r.include?(a)
         r.push(a)
       end
       i+=1
     end
     r
+  end
+
+  def actual_objects
+    self.playlist_items.map(&:resource_item).map(&:actual_object)
+  end
+
+
+  def child_playlists
+    arr = []
+    recursive_playlists(self){|x| arr << x}
+    arr = arr - [self]
+    arr
   end
 
   def collage_word_count
@@ -88,7 +101,7 @@ class Playlist < ActiveRecord::Base
     self.playlist_items.each do |pi|
       if pi.resource_item_type == 'ItemCollage' && pi.resource_item.actual_object
         shown_word_count += pi.resource_item.actual_object.words_shown.to_i
-        total_word_count += (pi.resource_item.actual_object.word_count.to_i-1) 
+        total_word_count += (pi.resource_item.actual_object.word_count.to_i-1)
       elsif pi.resource_item_type == 'ItemPlaylist' && pi.resource_item.actual_object
         res = pi.resource_item.actual_object.collage_word_count
         shown_word_count += res[0]
@@ -99,12 +112,12 @@ class Playlist < ActiveRecord::Base
   end
 
   def contains_item?(item)
-    actual_objects = self.playlist_items.inject([]) do |arr, pi| 
+    actual_objects = self.playlist_items.inject([]) do |arr, pi|
       arr << pi.resource_item.actual_object if pi.resource_item && pi.resource_item.actual_object; arr
     end
     actual_objects.include?(item)
   end
-  
+
   def push!(options = {})
     if options[:recipient]
       push_to_recipient!(options[:recipient])
@@ -115,69 +128,17 @@ class Playlist < ActiveRecord::Base
     else
       false
     end
-  end           
-  
+  end
+
   private
-  
-  def push_to_recipient!(recipient)
-     playlist = self
-     indent = "\t"
-                      
-     cloned_playlist = playlist.clone    
-     cloned_playlist.save!
-     cloned_playlist.accepts_role!(:owner, recipient)
-     cloned_playlist.accepts_role!(:creator, recipient)
-    #  Rails.cache.delete("user-playlists-#{recipient.id}")
-    #  cloned_playlist.tag_list = playlist.tag_list
-    # 
-    #  playlist.playlist_items.each do |pi|
-    #    cloned_playlist_item = pi.clone 
-    #    cloned_playlist_item.save!
-    #    cloned_playlist_item.accepts_role!(:owner, recipient)
-    #    cloned_resource_item = pi.resource_item.clone
-    #    cloned_resource_item.save!
-    #    cloned_resource_item.accepts_role!(:owner, recipient)
-    #    if pi.resource_item.actual_object
-    #      if pi.resource_item_type == 'ItemPlaylist'
-    #        puts "#{indent}cloning playlist: #{pi.resource_item.actual_object}"
-    #        cloned_object = deep_clone(pi.resource_item.actual_object, recipients, "#{indent}\t")
-    #      else
-    #        puts "#{indent}cloning item: #{pi.resource_item.actual_object}"
-    #        cloned_object = pi.resource_item.actual_object.clone
-    #        cloned_object.save!
-    #        cloned_object.accepts_role!(:owner, recipient)
-    #        cloned_object.accepts_role!(:creator, recipient)
-    #        cloned_object.tag_list = pi.resource_item.actual_object.tag_list
-    #        cloned_object.save
-    #        if pi.resource_item_type == 'ItemCollage'
-    #          pi.resource_item.actual_object.annotations.each do |annotation|
-    #          cloned_annotation = annotation.clone
-    #          cloned_annotation.collage_id = cloned_object.id
-    #          cloned_annotation.save
-    #          #cloned_annotation.tag_list = annotation.tag_list
-    #          #Assigning tag_list doesn't work for annotations
-    #          #So, work directly with taggings array of objects
-    #          annotation.taggings.each do |tagging|
-    #            cloned_tagging = tagging.clone
-    #            cloned_tagging.update_attribute(:taggable_id, cloned_annotation.id)
-    #          end
-    #        end
-    #        puts "#{indent}*cloned annotations"
-    #       end
-    #     end
-    #     cloned_resource_item.actual_object_id = cloned_object.id
-    #     cloned_resource_item.url = cloned_resource_item.url.gsub(/[0-9]+$/, cloned_object.id.to_s)
-    #   end
-    #   cloned_resource_item.save
-    #   cloned_playlist_item.position = pi.position
-    #   cloned_playlist_item.resource_item_id = cloned_resource_item.id
-    #   cloned_playlist_item.playlist_id = cloned_playlist.id
-    #   cloned_playlist_item.save
-    # end
-    # 
-    # cloned_playlist.save
-    # 
-    # return cloned_playlist
-    
+
+
+  private
+
+  def recursive_playlists(playlist)
+    yield playlist
+    playlist.actual_objects.find_all{|ao| ao.is_a?(Playlist)}.each do |child|
+      recursive_playlists(child){|x| yield x}
+    end
   end
 end
